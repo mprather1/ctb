f09ef6250bd87e9a0582718f47518d621fb26e01
Michael Prather <mprather1@gmail.com>
2016-04-17 20:42:48 -0500

    validations

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ var Person = Backbone.Model.extend({
     age: 30,
     occupation: "Worker"
   },
+
+  // validations on person.set must be called by using
+  // person.set('age', -50, { validate: true } )
+  validate: function(attrs) {
+    if ( attrs.age < 0 ) {
+      return 'Age must be positive!!!'
+    }
+
+    if ( ! attrs.name ) {
+      return 'Every person must have a name!!!'
+    }
+  },
+
   work: function() {
     return this.get('name') + " is working!!!"
   }
